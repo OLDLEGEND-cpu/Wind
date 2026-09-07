621df6a7d8b5d0e276061ac2b336a27f7d3dd3de
OLDLEGEND-cpu <OLDLEGEND-cpu@users.noreply.github.com>
2026-09-07 19:17:42 +0500

    Upgrade Netlify runtime to plugin-nextjs v5 with Node 20 and production environment

--- next.config.js
+++ next.config.js
@@ -4,18 +4,6 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['better-sqlite3', 'mysql2'],
     serverActions: { bodySizeLimit: '2mb' }
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      if (Array.isArray(config.externals)) {
-        config.externals.push('better-sqlite3', 'mysql2');
-      } else if (config.externals) {
-        config.externals = [config.externals, 'better-sqlite3', 'mysql2'];
-      } else {
-        config.externals = ['better-sqlite3', 'mysql2'];
-      }
-    }
-    return config;
   }
 };
 
